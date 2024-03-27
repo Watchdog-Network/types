c94201eb5a3200cc9e58512e9534715270141d61
jhikyuinn <hnn06228@naver.com>
2024-03-27 22:05:52 +0900

    data structure of fabric extension

---
+++ channeldata.go
@@ -0,0 +1,15 @@
+package types
+
+type FabricChannel struct {
+	Type         int               `bson:"type" json:"type"`
+	ChannelId    string            `bson:"channel" json:"channel"`
+	BlockNumber  int               `bson:"block" json:"block"`
+	Transactions int               `bson:"tx" json:"tx"`
+	Members      int               `bson:"members" json:"members"`
+	FabricPeers  []PeerAttribution `bson:"attr" json:"attr"`
+}
+
+type PeerAttribution struct {
+	Id   string
+	Sent int
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Watchdog-Network/types
+
+go 1.21.0
+
+require github.com/Watchdog-Network/gofountain v0.0.0-20240325014250-d79a17494f1b
