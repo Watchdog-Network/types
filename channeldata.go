bf26f29dd9f957fc5fde3dbdb85919a209a870f0
jhikyuinn <hnn06228@naver.com>
2024-03-29 17:51:48 +0900

    add channel data

--- channeldata.go
+++ channeldata.go
@@ -1,12 +1,13 @@
 package types
 
 type FabricChannel struct {
-	Type         int               `bson:"type" json:"type"`
-	ChannelId    string            `bson:"channel" json:"channel"`
-	BlockNumber  int               `bson:"block" json:"block"`
-	Transactions int               `bson:"tx" json:"tx"`
-	Members      int               `bson:"members" json:"members"`
-	FabricPeers  []PeerAttribution `bson:"attr" json:"attr"`
+	Type           int               `bson:"type" json:"type"`
+	ChannelId      string            `bson:"channel" json:"channel"`
+	BlockNumber    int               `bson:"block" json:"block"`
+	Transactions   int               `bson:"tx" json:"tx"`
+	Members        int               `bson:"members" json:"members"`
+	FabricPeers    []PeerAttribution `bson:"attr" json:"attr"`
+	ChannelTrigger chan string
 }
 
 type PeerAttribution struct {
